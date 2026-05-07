6208c2535fbe18347990b0b14934b565a60afdbd
Erick Costa do Nascimento <erickcosta2509@gmail.com>
2026-05-07 12:45:03 -0300

    initial configs

---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-zinc-950 text-white p-6">
+      <h1 className="text-4xl font-bold">DevBurger</h1>
+    </main>
+  )
+}
